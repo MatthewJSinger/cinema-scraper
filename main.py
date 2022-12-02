ff248ddbe369c8ad128d34976a1d138ee39a1de1
MatthewJSinger <matthewjsinger@hotmail.co.uk>
2022-12-02 16:30:24 +0000

    Cleaned up main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from CinemaScraper import Cinema 
 
 def run():
-    Cinema("Vue Cleveleys").printAllShowings()
-
+    Cinema("Vue Manchester").printAllShowings()
     
 if __name__ == '__main__':
     run()
