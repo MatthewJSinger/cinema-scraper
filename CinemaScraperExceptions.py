ccd2af34af577aa7c57fca511bc77f76fc2ebccc
MatthewJSinger <matthewjsinger@hotmail.co.uk>
2022-12-03 21:33:27 +0000

    Added exception for if there is no movie data (can occur with small indie cinemas)

--- CinemaScraperExceptions.py
+++ CinemaScraperExceptions.py
@@ -5,3 +5,11 @@ class CinemaNotFoundError(Exception):
 
     def __str__(self):
         return f'Cinema: {self.f} not found'
+
+class NoFilmsFoundError(Exception):
+    def __init__(self,f,*args):
+        super().__init__(args)
+        self.f = f
+
+    def __str__(self):
+        return f'No Films found while searching {self.f}. There may be no film data available for this Cinema'
